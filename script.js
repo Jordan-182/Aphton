11fa1fb0d622cd04ee0e6f928e3592187ec59080
Jordan-182 <jordan.p62@outlook.fr>
2024-08-17 20:42:14 +0200

    slick

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Animation de chaque lettre avec GSAP
     gsap.from(".slogan span", {
-        duration: 0.8,
+        duration: 1,
         opacity: 0,
         y: -20,
-        stagger: 0.05,
+        stagger: 0.03,
         ease: "power2.out",
         onStart: function() {
             // Faire apparaître le texte globalement au début de l'animation
@@ -41,3 +41,43 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+//---//
+
+$(document).ready(function(){
+    $('.avis-list').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+  });
