1eaafec790e369270448a312cedba68b369bd8b3
Jordan-182 <jordan.p62@outlook.fr>
2024-08-18 00:16:56 +0200

    18082024

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
 $(document).ready(function(){
     $('.avis-list').slick({
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
