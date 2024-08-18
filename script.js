5a0ca260533acb9e9b22ee9af4a859fd08d77d55
Jordan-182 <jordan.p62@outlook.fr>
2024-08-18 16:39:01 +0200

    18082024

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
         duration: 1,
         opacity: 0,
         y: -20,
-        stagger: 0.03,
+        stagger: 0.01,
         ease: "power2.out",
         onStart: function() {
             // Faire apparaître le texte globalement au début de l'animation
@@ -41,43 +41,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
-
-//---//
-
-$(document).ready(function(){
-    $('.avis-list').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
-  });
